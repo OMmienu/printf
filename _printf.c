3abe9856f94ffa3e630e15d46ab939d44f39a862
Ommienu <nkedzi@gmail.com>
2022-10-18 16:17:33 -0700

    printf project

--- _printf.c
+++ _printf.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * check_for_specifiers - checks if there is a valid format specifier
@@ -11,23 +11,22 @@ static int (*check_for_specifiers(const char *format))(va_list)
 {
 	unsigned int i;
 	print_t p[] = {
-		{"c", print_c};
-		{"s", print_s};
-		{"i", print_i};
-		{"d", print_d};
-		{"u", print_u};
-		{"b", print_b};
-		{"o", print_o};
-		{"x", print_x};
-		{"X", print_X};
-		{"p", print_p};
-		{"S", print_S};
-		{"r", print_r};
-		{"R", print_R};
+		{"c", print_c},
+		{"s", print_s},
+		{"i", print_i},
+		{"d", print_d},
+		{"u", print_u},
+		{"b", print_b},
+		{"o", print_o},
+		{"x", print_x},
+		{"X", print_X},
+		{"p", print_p},
+		{"S", print_S},
+		{"r", print_r},
+		{"R", print_R},
 		{NULL, NULL}
 	};
-	
-	for (i = 0; p[i].t !=NULL; i++)
+	for (i = 0; p[i].t != NULL; i++)
 	{
 		if (*(p[i].t) == *format)
 		{
@@ -43,7 +42,7 @@ static int (*check_for_specifiers(const char *format))(va_list)
  *
  * Return: number of characters printed
  */
-int _print(const char *format, ...)
+int _printf(const char *format, ...)
 {
 	unsigned int i = 0, count = 0;
 	va_list valist;
@@ -51,7 +50,7 @@ int _print(const char *format, ...)
 
 	if (format == NULL)
 		return (-1);
-	va_start(valist,format):
+	va_start(valist, format);
 	while (format[i])
 	{
 		for (; format[i] != '%' && format[i]; i++)
@@ -79,3 +78,4 @@ int _print(const char *format, ...)
 	}
 	va_end(valist);
 	return (count);
+}
